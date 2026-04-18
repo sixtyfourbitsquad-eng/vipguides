3d15918ea1fe3a952bd041f883b922249a1145ff
sixtyfourbitsquad-eng <sixtyfourbitsquad@gmail.com>
2026-04-18 18:13:56 +0530

    Update route.ts

--- app/api/goto/route.ts
+++ app/api/goto/route.ts
@@ -3,6 +3,6 @@ import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
-  const url = process.env.SECRET_AD_LINK || "https://wa.me/";
+  const url = process.env.SECRET_AD_LINK || "https://wa.me/9156689574";
   return NextResponse.redirect(url, 302);
 }
